b68ac7463bef9541e70083d1ba2fde53adec41f7
Praful Aswale <prafulcode15@gmail.com>
2023-03-24 08:06:27 +0530

    fetching commets

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,16 @@
+const Post = require("../models/post");
+
 module.exports.home = function (req, res) {
-  return res.render("home", {
-    title: "Home",
-  });
+  // Post.find({}).then();
+
+  //populate the
+  Post.find({})
+    .populate("user")
+    .exec()
+    .then(function (posts) {
+      return res.render("home", {
+        title: "Codeial | Home",
+        posts: posts,
+      });
+    });
 };
