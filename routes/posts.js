a89aeb585fb05fac04b216eb34ba01835d456e4e
Praful Aswale <prafulcode15@gmail.com>
2023-03-24 08:23:35 +0530

    authentication for posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
 
 const posts = require("../controllers/posts");
 
-router.post("/create", posts.create);
+router.post("/create", passport.checkAuthentication, posts.create);
 
 module.exports = router;
